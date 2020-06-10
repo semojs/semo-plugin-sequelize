12663c20e11f813d7715d0b211c83ae22ff011d9
Richard Yu <vipzhicheng@gmail.com>
2020-06-10 10:48:10 +0800

    fix: command args

--- src/extends/semo/src/commands/generate/migration.ts
+++ src/extends/semo/src/commands/generate/migration.ts
@@ -1,6 +1,6 @@
 import * as migrationCommand from '../../../../../commands/sequelize/generate'
 
-export const command = 'migration <dbKey> <tableName> [fieldName]'
+export const command = 'migration <tableName> [fieldName]'
 export const desc = 'Generate a Sequelize migration'
 export const aliases = ['mig']
 
