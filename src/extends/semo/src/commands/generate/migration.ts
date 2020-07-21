a267520d07210c27d50ec1f920b1244e77b2dab1
Richard Yu <vipzhicheng@gmail.com>
2020-07-21 16:42:59 +0800

    fix: fetch wrong plugin config

--- src/extends/semo/src/commands/generate/migration.ts
+++ src/extends/semo/src/commands/generate/migration.ts
@@ -1,5 +1,6 @@
 import * as migrationCommand from '../../../../../commands/sequelize/generate'
 
+export const plugins = 'sequelize'
 export const command = 'migration <tableName> [fieldName]'
 export const desc = 'Generate a Sequelize migration'
 export const aliases = ['mig']
